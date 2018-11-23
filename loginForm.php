a9902b396d0a69bdb763ca7bec2c8d03be229b2a
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-11-23 18:58:20 +0100

    Añadido el create table

---
+++ loginForm.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+    <?php
+        echo "<h3>LOG IN</h3>";
+        echo "<form action='' method='post'>";
+            echo "User name: <input type='text' name='user' value='' placeholder='username'><br>";
+            echo "Password: <input type='password' name='user' value='' placeholder='password'><br>";
+            echo "<input type='submit' name='submit'>";
+        echo "</form>";
+
+
+        if ($_POST != null) {
+            # Conexión a la bdd
+            $conn = mysqli_connect('localhost','jose','jose123');
+            mysqli_select_db($conn, 'Usuarios');
+            $consulta = "SELECT * FROM Usuarios;";
+            $resultat = mysqli_query($conn, $consulta);
+            if (!$resultat) {
+                $message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
+                $message .= 'Consulta realitzada: ' . $consulta;
+                die($message);
+            }
+        }
+    ?>
+    </body>
+</html>
